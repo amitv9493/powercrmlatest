182ddd23446a3ac2adbfff92d5b902029ea693a0
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-06-23 09:28:10 +0530

    modify main urls.py

--- powercrm/urls.py
+++ powercrm/urls.py
@@ -3,13 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = (
-    [
-        path("sites/", include("sites.urls")),
-        path("admin", admin.site.urls),
-        path("api/", include("api.urls")),
-       
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("sites/", include("sites.urls")),
+    path("admin", admin.site.urls),
+    path("api/", include("api.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
