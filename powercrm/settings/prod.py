ef23d210198f7fde9d963bc0fde42a108a764d75
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-06-29 13:12:28 +0530

    update rest_framework settings in prod

--- powercrm/settings/prod.py
+++ powercrm/settings/prod.py
@@ -18,3 +18,25 @@ STATIC_ROOT = config("STATIC_ROOT")
 MEDIA_ROOT = config("MEDIA_ROOT")
 
 MEDIA_URL = config("MEDIA_URL")
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissions",
+        "rest_framework.permissions.IsAdminUser",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
