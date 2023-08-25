07a3f1974315ddde2cd7612699c4021a0d40fbd2
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-08-25 14:39:44 +0530

    optimize query for multisite

--- multisite/serializers.py
+++ multisite/serializers.py
@@ -2,20 +2,23 @@ from rest_framework import serializers
 from .models import MultiSite
 from sites.serializers import Site_Create_Serializer
 from sites.models import Site
+
 class Site_Serializer(serializers.ModelSerializer):
+    company = serializers.StringRelatedField()
+    parent_company = serializers.StringRelatedField()
     class Meta:
         model = Site
-        fields = ("id", "site_name", "company", "parent_company")
+        fields = ("id", "site_name", "company", "parent_company",)
         
 class MultiSiteSerializer(serializers.ModelSerializer):
     # sites = Site_Serializer(many=True)
-
+    sites = serializers.SerializerMethodField
     class Meta:
         model = MultiSite
         fields = "__all__"
     
     def to_representation(self, instance):
         response = super().to_representation(instance)
-        serializer = Site_Serializer 
+        serializer = Site_Serializer
         response['sites'] = serializer(instance.sites.all(), many=True).data
         return response
--- multisite/views.py
+++ multisite/views.py
@@ -1,13 +1,19 @@
 # Create your views here.
 
 from rest_framework.viewsets import ModelViewSet
-
+from django.db.models import Prefetch
 from .models import MultiSite
 from .serializers import MultiSiteSerializer
 from api.paginator import CustomPagination
-
+# from silk.profiling.profiler import silk_profile
+from sites.models import Site
 
 class MultiSiteViewSet(ModelViewSet):
-    queryset = MultiSite.objects.all()
+    queryset = MultiSite.objects.prefetch_related(
+        Prefetch(lookup="sites",queryset=Site.objects.only("id", "company", "parent_company").select_related("company")))
     serializer_class = MultiSiteSerializer
     pagination_class = CustomPagination
+    
+    # @silk_profile(name='MultiSiteViewSet list')
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
