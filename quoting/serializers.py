d56f13742b48b62bcbccbc0985f1714a628e574b
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-20 12:04:32 +0530

    Update group quote

--- quoting/serializers.py
+++ quoting/serializers.py
@@ -20,17 +20,8 @@ class QuoteSettingSerializer(serializers.ModelSerializer):
 
 
 class GroupQuoteSerializer(serializers.ModelSerializer):
-    # group_detail = serializers.HyperlinkedIdentityField(
-    #     view_name="general-quote", read_only=True, lookup_field="pk"
-    # )
-    group_detail = serializers.HyperlinkedRelatedField(
-        view_name="general-quote",
-        many=True,
-        read_only=True,
-        lookup_field="pk",
-    )
 
     class Meta:
         model = Generate_Group_Quote
-        fields = ["group_detail"]
+        fields = "__all__"
 
