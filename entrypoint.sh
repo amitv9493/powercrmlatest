ec629cc2d95a0b5fa259132c1145e1a75c0835a1
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-05 13:40:46 +0530

    dockerize project

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --noinput
-gunicorn powercrm.wsgi:powercrm.wsgi:application --bind 0.0.0.0:8000
+gunicorn /app/powercrm.wsgi:application --bind 0.0.0.0:8000
 
